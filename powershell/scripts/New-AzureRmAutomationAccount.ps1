bf1877a2719665616a86154b09480427f830332c
Mike Jones <github@smenzo.id.au>
2016-09-18 09:48:13 +1000

    Create scripts and samples folders

---
+++ powershell/scripts/New-AzureRmAutomationAccount.ps1
@@ -0,0 +1,13 @@
+﻿# Create new Azure Automation Account
+
+$AutomationAccountName  = "AutomationDemo1"
+$ResourceGroupName      = "AutomationDemo1RG"
+$Location               = "Australia Southeast"
+
+
+Login-AzureRmAccount
+
+
+New-AzureRmResourceGroup –Name $ResourceGroupName –Location "$Location"
+
+New-AzureRmAutomationAccount –Name $AutomationAccountName –ResourceGroupName $ResourceGroupName –Location "$Location"
