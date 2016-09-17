52ba636390ffd26a5b0c50b10a032146abaa2040
Mike Jones <mikejonestechno@users.noreply.github.com>
2016-09-17 22:45:52 +1000

    Replace variables with params

--- powershell/import-AzureRmAutomationDscConfiguration.ps1
+++ powershell/import-AzureRmAutomationDscConfiguration.ps1
@@ -1,18 +1,25 @@
-$myAutomationRG = "AzureDSC"
-$myAutomationAccount = "AzureDSC"
-$sourcePath = "dsc"
-$configurationName = "SonarQube"
+# Import a DSC configuration to Azure and compile it so it can be assigned to a node
+
+param(
+    [Parameter(Mandatory=$true)]
+    [String] $AutomationAccountName,
+    
+    [Parameter(Mandatory=$true)]
+    [String] $ResourceGroupName,
+   
+    [Parameter(Mandatory=$true)]
+    [String] $ConfigName  # Config file name must match the name of the configuration
+ )
 
 Login-AzureRmAccount
 
-Import-AzureRmAutomationDscConfiguration `
-    -SourcePath "$sourcePath\$configurationName.ps1" `
-    -ResourceGroupName $myAutomationRG –AutomationAccountName $myAutomationAccount  `
-    -Published –Force
+Import-AzureRmAutomationDscConfiguration -SourcePath "dsc\$ConfigName.ps1" -Published –Force `
+  –AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName 
 
-$jobData = Start-AzureRmAutomationDscCompilationJob -ConfigurationName $configurationName `
-    -ResourceGroupName $myAutomationRG –AutomationAccountName $myAutomationAccount 
+$jobData = Start-AzureRmAutomationDscCompilationJob -ConfigurationName $ConfigName `
+  –AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName 
 
 $compilationJobId = $jobData.Id
 
-Get-AzureRmAutomationDscCompilationJob -ResourceGroupName $myAutomationRG –AutomationAccountName $myAutomationAccount -Id $compilationJobId
+Get-AzureRmAutomationDscCompilationJob -Id $compilationJobId `
+  –AutomationAccountName $AutomationAccountName -ResourceGroupName $ResourceGroupName 
