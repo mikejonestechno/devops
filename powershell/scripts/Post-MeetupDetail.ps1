d25dea1fb3336d122bfe4922d2cfacfebcfbdcdd
Mike Jones <mikejonestechno@users.noreply.github.com>
2017-03-26 18:02:44 +1100

    Add MS Teams to meetup.com connector
    Adding Post-MeetupDetail.ps1 and Post-MeetupSummary.ps1

---
+++ powershell/scripts/Post-MeetupDetail.ps1
@@ -0,0 +1,88 @@
+﻿# Loop through the groups, if there is a meetup within next 24 hours then post it!
+# Observed that the meetup group photo is squished to square shape in Teams. Not sure there is much we can do about that!
+
+$TeamMeetups = (@{ 'TeamName' = 'MSTeamExperiments.TestChannel1'; # human readable name just to make it easier to manage this hastable
+    'Webhook' = 'https://outlook.office.com/webhook/<your-secure-string-here>';
+    'MeetupGroups' = @("MelbourneAzure","devops-melbourne","Melbourne-Powershell-Meetup","Infrastructure-Coders","Melbourne-Serverless-Meetup-Group","Melbourne-Microsoft-Cloud-and-Datacenter-Meetup", #devops
+        "Melbourne-ALT-NET","Melbourne-Microservices","Chatbots-Messaging-and-AI-Melbourne","Melbourne-Web-Developers","Melbourne-TechTalk", # programming
+        "TestEngineeringAlliance","Melbourne-Software-Testing-Meetup-Group","Melbourne-Software-Testing-Meetup", # testing
+        "scrum-12","Enterprise-Innovation-Meetup","Limited-WIP-Society","AgileCoach","Australia-Delivery-Leads") # agile
+    },
+    @{ 'TeamName' = 'MSTeamExperiments.TestChannel2'; 
+    'Webhook' = 'https://outlook.office.com/webhook/<your-secure-string-here>';
+    'MeetupGroups' = @("devops-sydney","Azure-Sydney-User-Group","Cloud-Engineering-Sydney", #devops
+        "Sydney-Microsoft-User-Group", # programming
+        "Sydney-Testers-and-Automators",
+        "Continuous-Delivery-Sydney","Sydney-Scrum","Sydney-Agile-Scrum-Meetup","Sydney-Agile-Coach-Meetup","Agile-Sydney") # agile
+    }
+)
+
+ForEach ($Team in $TeamMeetups) {
+    Write-Output "MS Team: $($Team.TeamName)"
+    ForEach ($MeetupGroup In $Team.MeetupGroups) {
+
+        Write-Output "MeetupGroup: $MeetupGroup"
+        $event = $null
+        $body = @()
+        $uri = "https://api.meetup.com/$MeetupGroup/events?&sign=true&photo-host=public&scroll=next_upcoming&page=1&fields=group_photo,venue_visibility,visibility"
+
+        try {
+            $event = Invoke-RestMethod -Uri $Uri -ContentType 'application/json' -Method Get
+        }
+        catch [System.Exception] {
+            throw $_
+        }
+
+        $event.group.photo
+
+        $eventTime = (Get-Date 1/1/1970).AddSeconds($event.time/1000).ToLocalTime()
+
+        If ($eventTime -ge (Get-Date) -and $eventTime -le (Get-Date).AddDays(1)) {
+   
+            Write-Output "Meeting soon! Lets message the Team!" 
+
+            <# Uuurrrrgggghhh!
+             # Fancy quotes break the payload. Even $_.plain_text_description can contain fancy quotes!   
+             # Also [char]160 is a non-breaking space char which doesnt break payload but turns into ? char when pushed to Teams
+             # http://stackoverflow.com/questions/27020671/remove-xa0-from-powershell-string
+             #>
+            If ($event.visibility -eq 'public') {
+                $unescapeDesc = ([regex]"[\u0080-\uffff]").Replace($event.description, { param($m) "&#$([int][char]$m.Value);" })
+            } Else {
+                $unescapeDesc = ('Meeting details are shown only to members of the ' + $event.group.name)
+            }
+            # Generate the same map link that meetup.com display on their pages
+            If ($event.venue_visibility -eq 'public') {
+                $map ="https://maps.google.com/maps?f=q&hl=en&q=" + [System.Web.HttpUtility]::UrlEncode($event.venue.name + ',' + $event.venue.address_1 + ',' + $event.venue.city + ',' + $event.venue.country)
+                $venue = ($event.venue.name + ', ' +  $event.venue.city + ' ([View Map](' + $map + '))' )
+            } Else {
+                $venue = ('Location is shown only to members of the ' + $event.group.name)
+            }
+
+            $body = @{    Title = ('Reminder: ' + $event.name + ' at ' + $eventTime.ToString("h:mm tt") )
+                Summary = "Reminder for upcoming event"
+                themeColor = "E81123"
+                Sections = @(
+                @{
+                ActivityTitle = '[' + $event.group.name + '](' + $event.link + ')'
+                ActivitySubtitle = '**' + ($eventTime.ToString("dddd d MMMM h:mm tt") + '** (' + $event.yes_rsvp_count + ' are going)' )
+                ActivityText = $venue
+                ActivityImage = ($event.group.photo.thumb_link)
+                Text = '## Like this message if you are going! @mention others that may be interested!'
+                }
+                @{ Text = ($unescapeDesc)
+                }
+               )
+            }
+
+            # Post message to Teams webhook
+            try {
+                Invoke-RestMethod -Method Post -Uri ($Team.Webhook) -Body (ConvertTo-Json $body -Depth 4) 
+            }
+            catch [System.Exception] {
+                throw $_
+            }
+        }
+    }
+}
+
---
+++ powershell/scripts/Post-MeetupSummary.ps1
@@ -0,0 +1,64 @@
+﻿# Loop through the groups, if there is a meetup within next 24 hours then post it!
+# Observed that the meetup group photo is squished to square shape in Teams. Not sure there is much we can do about that!
+
+$TeamMeetups = (@{ 'TeamName' = 'MSTeamExperiments.TestChannel1'; 
+    'Webhook' = 'https://outlook.office.com/webhook/<your-secure-string-here>';
+    'MeetupGroups' = @("MelbourneAzure","devops-melbourne","Melbourne-Powershell-Meetup","Infrastructure-Coders","Melbourne-Serverless-Meetup-Group","Melbourne-Microsoft-Cloud-and-Datacenter-Meetup", #devops
+        "Melbourne-ALT-NET","Melbourne-Microservices","Chatbots-Messaging-and-AI-Melbourne","Melbourne-Web-Developers","Melbourne-TechTalk", # programming
+        "TestEngineeringAlliance","Melbourne-Software-Testing-Meetup-Group","Melbourne-Software-Testing-Meetup", # testing
+        "scrum-12","Enterprise-Innovation-Meetup","Limited-WIP-Society","AgileCoach","Australia-Delivery-Leads") # agile
+    },
+    @{ 'TeamName' = 'MSTeamExperiments.TestChannel2'; 
+    'Webhook' = 'https://outlook.office.com/webhook/<your-secure-string-here>';
+    'MeetupGroups' = @("devops-sydney","Azure-Sydney-User-Group","Cloud-Engineering-Sydney", #devops
+        "Sydney-Microsoft-User-Group", # programming
+        "Sydney-Testers-and-Automators",
+        "Continuous-Delivery-Sydney","Sydney-Scrum","Sydney-Agile-Scrum-Meetup","Sydney-Agile-Coach-Meetup","Agile-Sydney") # agile
+    }
+)
+
+ForEach ($Team in $TeamMeetups) {
+    Write-Output "MS Team: $($Team.TeamName)"
+    $events = @()
+    ForEach ($MeetupGroup In $Team.MeetupGroups) {
+        Write-Output "MeetupGroup: $MeetupGroup"
+        $uri = "https://api.meetup.com/$MeetupGroup/events?&sign=true&photo-host=public&scroll=next_upcoming&page=3&fields=group_photo,venue_visibility,visibility"
+        try {
+            $events += Invoke-RestMethod -Uri $Uri -ContentType 'application/json' -Method Get
+        }
+        catch [System.Exception] {
+            throw $_
+        }
+    }
+
+    $upcomingEvents = $events | Sort-Object time | Select-Object -First 12 # Maximum number of events to show in summary
+
+    $maxLength = 90 # trial end error found that the short date format leaves 90 chars before value word wraps
+    $fact = @()
+    ForEach ($event In $upcomingEvents) {
+        $eventTime = (Get-Date 1/1/1970).AddSeconds($event.time/1000).ToLocalTime()
+        If ($eventTime -ge (Get-Date) -and $eventTime -le (Get-Date).AddDays(30)) { 
+            $eventInfo = ($event.group.name + ': [' + $event.name )       
+            If ($eventInfo.Length -gt $maxLength) { # try shortening the group name
+                $shortGroupName = ($event.group.name -replace 'user|' -replace 'group|' -replace 'meetup|').Trim()
+                $eventInfo = ($shortGroupName + ': [' + $event.name )
+                If ($eventInfo.Length -gt $maxLength) { # meh, just truncate it
+                    $eventInfo = ($eventInfo.substring(0,$maxLength-1) + '...')
+                }
+            }
+            $fact += @{name = ($eventTime.ToString("ddd dd MMM HH:mm").ToUpper()); value = ($eventInfo + '](' + $event.link + ')' )} 
+        }
+    }
+
+    # Message with just Facts will show 6 meetings with a 'see more' if there are more.
+    $body = @{    
+        Text = "Check out these upcoming events!"
+        Sections = @(
+        @{    
+        Facts = @($fact)
+        }
+       )
+    }
+    Invoke-RestMethod -Method Post -Uri ($Team.Webhook) -Body (ConvertTo-Json $body -Depth 4) 
+
+}
